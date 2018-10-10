50a9be3f9141aa5c0908a672b142f5f6ef814b72
水野淳 <>
2018-10-10 17:48:46 +0900

    Punの同期テストの準備

---
+++ unity_pun/Assets/Scripts/PhotonUnityNetworkController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonUnityNetworkController : MonoBehaviour {
+    /// <summary>
+    /// 開始時
+    /// </summary>
+	void Start () {
+        // マスターサーバーへ接続
+        PhotonNetwork.OfflineMode = false;
+        PhotonNetwork.ConnectUsingSettings();
+	}
+	
+    /// <summary>
+    /// 更新時
+    /// </summary>
+	void Update () {
+		
+	}
+}
